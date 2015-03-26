e6452fdb1885adc9993bdb8205225caeacee6902
hp <hp@172.16.87.199>
2015-03-26 19:43:44 +0100

    login
    Signed-off-by: hp <hp@172.16.87.199>

--- game-tube/game-tube-web/src/main/java/mBeans/LoginBean.java
+++ game-tube/game-tube-web/src/main/java/mBeans/LoginBean.java
@@ -1,14 +1,35 @@
 package mBeans;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import services.interfaces.UserManagementServicesLocal;
+import domain.Player;
 import domain.User;
 
 @ManagedBean
 @SessionScoped
 public class LoginBean {
 	private User user = new User();
+	@EJB
+	private UserManagementServicesLocal userManagementServicesLocal;
+
+	public String doLogin() {
+		User userFounded = userManagementServicesLocal.login(user.getLogin(),
+				user.getPassword());
+		if (userFounded != null) {
+			if (userFounded instanceof Player) {
+				return "/pages/mainRoom";
+			} else {
+				return "";
+			}
+
+		} else {
+			return "/error";
+		}
+
+	}
 
 	public User getUser() {
 		return user;
