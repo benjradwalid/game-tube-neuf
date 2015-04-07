3af70377f32cca015660685581c546ee361b0726
bilel <bilel@172.16.1.94>
2015-04-07 18:24:05 +0000

    update score

---
+++ game-tube/game-tube-web/src/mBeans/DataListBean.java
@@ -0,0 +1,53 @@
+package mBeans;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+
+
+import services.interfaces.GameManagementServicesLocal;
+import domain.Player;
+
+@ManagedBean
+@ViewScoped
+public class DataListBean implements Serializable {
+     
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+	private List<Player> player;
+     
+     
+    @ManagedProperty("#{playerService}")
+    private GameManagementServicesLocal service;
+     
+    @PostConstruct
+    public void init() {
+        player = service.getPlayers();
+     
+    }
+ 
+    public List<Player> getPlayers() {
+        return player;
+    }  
+ 
+    public void setService(GameManagementServicesLocal service) {
+        this.service = service;
+    }
+ /*
+    public Car getSelectedCar() {
+        return selectedCar;
+    }
+ 
+    public void setSelectedCar(Car selectedCar) {
+        this.selectedCar = selectedCar;
+    }*/
+}
